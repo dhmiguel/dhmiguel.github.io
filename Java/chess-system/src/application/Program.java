d5850450dcf1db7c2f99834f2730ada917d456ed
Diego Miguel <diegomiguel.201@gmail.com>
2020-05-19 10:50:47 -0300

    add First class: Position and test this class

--- Java/chess-system/src/application/Program.java
+++ Java/chess-system/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,7 +12,8 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello World testing");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 		sc.close();
 
