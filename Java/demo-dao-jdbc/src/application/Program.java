36a2dbca4b60640b62c6518fb604d62fe91abaae
Diego Miguel <diegomiguel.201@gmail.com>
2020-07-19 14:34:31 -0300

    add Update method implementation

--- Java/demo-dao-jdbc/src/application/Program.java
+++ Java/demo-dao-jdbc/src/application/Program.java
@@ -46,6 +46,12 @@ public class Program {
 		
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
+		System.out.println("\n --- Test 5: seller Update ---");
+
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
 
 }
